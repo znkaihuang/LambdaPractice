e1c852f9b1e11c77ebd05d1328645535d2703712
znkaihuang <zkhuang@protonmail.com>
2022-05-29 16:37:55 +0800

    Commit LocalePractice

---
+++ LocalePractice/src/com/lessayer/practice/LocalePractice.java
@@ -0,0 +1,65 @@
+package com.lessayer.practice;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Locale.LanguageRange;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class LocalePractice {
+	
+	public static void main(String... args) {
+		Map<String, String> languageTagsMap = createLanguageTagMap();
+		List<Locale> localeList = createLocaleList(languageTagsMap.values());
+		List<LanguageRange> priorityList = LanguageRange.parse("jp, zh-tw;q=0.65, en;q=0.5, en-US;q=0.5, fr;q=0.4, fr-ca;q=0.4, es;q=0.2, it;q=0.1");
+		
+		System.out.println(selectClosetLocale("jp", priorityList, localeList));
+		System.out.println(selectClosetLocale("de", priorityList, localeList));
+		System.out.println(selectClosetLocale("en", priorityList, localeList));
+		System.out.println(selectClosetLocale("fr", priorityList, localeList));
+		System.out.println(selectClosetLocale("es", priorityList, localeList));
+		System.out.println(selectClosetLocale("fr-ca", priorityList, localeList));
+		System.out.println(selectClosetLocale("it", priorityList, localeList));
+		
+		printGreetings(selectClosetLocale("jp", priorityList, localeList));
+	}
+	
+	public static Map<String, String> createLanguageTagMap() {
+		Map<String, String> languageTagMap = new HashMap<>();
+		languageTagMap.put("Traditional_Chinese", "zh-TW");
+		languageTagMap.put("English", "en-US");
+		languageTagMap.put("American_English", "en-US");
+		languageTagMap.put("French", "fr");
+		languageTagMap.put("Canadian_French", "fr-CA");
+		languageTagMap.put("Spanish", "es");
+		
+		return languageTagMap;
+	}
+	
+	public static List<Locale> createLocaleList(Collection<String> collection) {
+		List<Locale> localeList = new ArrayList<>();
+		collection.forEach(tag -> localeList.add(Locale.forLanguageTag(tag)));
+		
+		return localeList;
+	}
+	
+	public static void printGreetings(Locale locale) {
+		ResourceBundle messageBundle = ResourceBundle.getBundle("language_bundles/MessagesBundle", locale);
+		System.out.println("=====" + messageBundle.getString("greetings") + "=====");
+		Integer count = 1;
+		for (String key : messageBundle.keySet()) {
+			if (!key.equals("greetings")) {
+				System.out.println(count++ + ": " + messageBundle.getString(key));
+			}
+		}
+	}
+	
+	public static Locale selectClosetLocale(String languageTag, List<LanguageRange> priorityList, List<Locale> localeList) {
+		// Complete your code here!
+		return null;
+	}
+	
+}
